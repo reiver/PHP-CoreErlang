cd633d06e050fce0be2fd0d1436efe750143236c
Charles <charles@reptile.ca>
2012-10-15 22:11:29 -0700

    added exception class

---
+++ lib/CoreErlang/Exception.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace CoreErlang;
+
+class Exception extends \Exception
+{
+    public function __construct($message = null, $code = 0, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public static function throwIfNotInstanceOf($expected, $actual, $message = null, $code = 0, Exception $previous = null)
+    {
+        if (  ! $actual instanceof $expected  ) {
+    /////// THROW
+            throw new static($message, $code, $previous);
+        }
+    }
+
+    public static function throwIfInstanceOf($expected, $actual, $message = null, $code = 0, Exception $previous = null)
+    {
+        if (  $actual instanceof $expected  ) {
+    /////// THROW
+            throw new static($message, $code, $previous);
+        }
+    }
+}
