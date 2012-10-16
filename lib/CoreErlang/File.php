7132244fb5b7a4fd7a6bb0d03bfba541e0dfacff
Charles <charles@reptile.ca>
2012-10-15 22:28:52 -0700

    added File class. (depends on things still not committed.)

---
+++ lib/CoreErlang/File.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace CoreErlang;
+
+class File
+{
+    private $module;
+
+    private $name;
+    private $indentation = "\t";
+
+
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+
+
+    public function module()
+    {
+        $this->module = new Module($this, $this->name);
+
+        return $this->module;
+    }
+
+
+
+    public function compileToCoreFile()
+    {
+        $file_name = $this->name .'.core';
+        $fd = fopen($file_name, 'w');
+
+        Exception::throwIfEquals(false, $fd);
+
+        $this->module->compile($fd);
+    }
+
+
+
+    public function indentation($n=1)
+    {
+        return str_repeat($this->indentation, $n);
+    }
+}
+
