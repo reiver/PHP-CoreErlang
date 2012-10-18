69b37c334ef21d167c51f00260d897315ba4841a
Charles <charles@reptile.ca>
2012-10-18 06:44:02 -0700

    added String class and unit test for it

---
+++ tests/lib/CoreErlang/StringTest.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once(  __DIR__ .'/../../CoreErlangProgenitorTestCase.php'  );
+
+class StringTest extends CoreErlangProgenitorTestCase
+{
+    public function testCreate()
+    {
+        list($file, $module) = $this->generateFileAndModule();
+
+        //@TODO - Really, this isn't a valid progenitor.
+        $progenitor_value = 'testin123'.uniqid();
+        $progenitor = new CoreErlang\String($file, $module, null, $progenitor_value);
+
+        $obj_value = 'testin123'.uniqid();
+        $obj    = new CoreErlang\String($file, $module, $progenitor, $obj_value);
+
+        $results = array($file, $module, $progenitor, $obj);
+
+        return $results;
+    }
+
+
+
+    public function testGetAndSetValue()
+    {
+        list($file, $module) = $this->generateFileAndModule();
+
+        $value1 = 'testin123'.uniqid();
+
+        $string = new CoreErlang\String($file, $module, null, $value1);
+
+        $this->assertEquals($value1, $string->getValue());
+
+
+
+        $value2 = 'anothertestin4567'.uniqid();
+
+        $string->setValue($value2);
+
+        $this->assertEquals($value2, $string->getValue());
+    }
+
+
+
+    public function testCompilePre()
+    {
+        list($file, $module) = $this->generateFileAndModule();
+        $value1 = 'testin123'.uniqid();
+        $obj = new CoreErlang\String($file, $module, null, $value1);
+
+//@TODO - Is this the proper way to escape a string in Erlang.
+        $expected_compiled_contents = '\''. $obj->getValue() .'\'';
+
+        $results = array($obj, $expected_compiled_contents);
+
+        return $results;
+    }
+}
