b57b770c3cab2dc1628b9c4267da1ba10db5cea7
Charles <charles@reptile.ca>
2012-10-17 20:59:47 -0700

    added static methods to handle floats in Exception class

--- lib/CoreErlang/Exception.php
+++ lib/CoreErlang/Exception.php
@@ -80,4 +80,22 @@ class Exception extends \Exception
             throw new static($message, $code, $previous);
         }
     }
+
+
+
+    public static function throwIfIsNotFloat($actual, $message = null, $code = 0, \Exception $previous = null)
+    {
+        if (  !is_float($actual)  ) {
+    /////// THROW
+            throw new static($message, $code, $previous);
+        }
+    }
+
+    public static function throwIfIsFloat($actual, $message = null, $code = 0, \Exception $previous = null)
+    {
+        if (  is_float($actual)  ) {
+    /////// THROW
+            throw new static($message, $code, $previous);
+        }
+    }
 }
