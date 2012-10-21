00cc5ecc0a4edec4ebb7ee10cbbb02610e326a45
Charles <charles@reptile.ca>
2012-10-20 22:49:57 -0700

    dev work on EmptyList

--- lib/CoreErlang/EmptyList.php
+++ lib/CoreErlang/EmptyList.php
@@ -2,7 +2,7 @@
 
 namespace CoreErlang;
 
-class EmptyList extends Progenitor implements ICompile, IConstant, ILit
+class EmptyList extends Progenitor implements ICompile, IConstant, IList, ILit
 {
     public function __construct(File $file, Module $module, Progenitor $progenitor = null)
     {
@@ -18,6 +18,30 @@ class EmptyList extends Progenitor implements ICompile, IConstant, ILit
     public function setValue($value)
     {
         Exception::throwIfNotEquals(array(), $value);
+    }
+
+
+
+    public function getHead()
+    {
+        throw Exception('Undefined');
+    }
+    public function setHead($vhead)
+    {
+        throw Exception('Undefined');
+
+        return $this;
+    }
+
+
+
+    public function getTail()
+    {
+        throw Exception('Undefined');
+    }
+    public function setTail($tail)
+    {
+        throw Exception('Undefined');
 
         return $this;
     }
